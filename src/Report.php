9a82a7b6f2e6e60f4db74a83f274e6b084e84eba
ayrokid <ayrokid@gmail.com>
2022-10-25 14:15:30 +0700

    Update Report

--- src/Report.php
+++ src/Report.php
@@ -9,9 +9,17 @@ class Report
 {
     use Server;
 
-    public function real_time()
+    public function real_time($data=null)
     {
-        $url = self::api()."report-realtime";
+        $id='';
+        $page='';
+        if(isset($data['message_id'])){
+            $id = $data['message_id'];
+        };
+        if(isset($data['page'])){
+            $page = $data['page'];
+        };
+        $url = self::api()."report-realtime?message_id=$id&page=$page";
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
             'Authorization'=> self::token()
